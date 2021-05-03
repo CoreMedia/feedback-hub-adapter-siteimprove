70a305ab6dac3c9a5c3577bc6c1e805875135c72
Matthias Faust <matthias.faust@coremedia.com>
2021-05-03 10:51:01 +0200

    2104.1 reset

--- cae/src/main/resources/META-INF/resources/WEB-INF/templates/common/com.coremedia.blueprint.siteimprove/SiteImproveMetadata.head.ftl
+++
@@ -1,2 +0,0 @@
-<#-- @ftlvariable name="self" type="com.coremedia.blueprint.siteimprove.SiteImproveMetadata" -->
-<meta name="coremedia:content-id" content="${self.contentId}" />
--- studio-server/src/main/java/com/coremedia/blueprint/feedbackhub/siteimprove/SiteimproveFeedbackTabs.java
+++
@@ -1,9 +0,0 @@
-package com.coremedia.blueprint.feedbackhub.siteimprove;
-
-/**
- *
- */
-public interface SiteimproveFeedbackTabs {
-  String PREVIEW = "preview";
-  String COMPARISON = "comparison";
-}

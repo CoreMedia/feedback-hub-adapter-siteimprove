6b9e3387096a28819c3cf036050d85da381a30ae
phempten <Olaf2020&>
2021-05-12 15:47:53 +0200

    SALESDEMO-2698: Fixed and cleaned up Content Link Builder. Using POST requests to retrieve preview and live URLs only.

--- feedback-hub-siteimprove-studio-lib/src/main/java/com/coremedia/blueprint/feedbackhub/siteimprove/service/ContentLinkBuilder.java
+++ feedback-hub-siteimprove-studio-lib/src/main/java/com/coremedia/blueprint/feedbackhub/siteimprove/service/ContentLinkBuilder.java
@@ -31,26 +31,7 @@ public class ContentLinkBuilder {
 
   @Nullable
   String buildPreviewLink(String baseUrl, String id) {
-    String url = baseUrl;
-    if (!url.endsWith("/")) {
-      url = url + "/";
-    }
-    String serviceUrl = url + PREVIEW_URL_SERVICE_URL;
-
-    UriComponents uriComponents = UriComponentsBuilder.fromUriString(serviceUrl)
-            .queryParam("id", IdHelper.parseContentId(id)).build();
-
-    String link = getLink(uriComponents.toUriString());
-    if (link != null && !link.startsWith("http")) {
-      link = uriComponents.getScheme() + ":" + link;
-    }
-
-    return link;
-  }
-
-  @Nullable
-  String buildLiveLink(String baseUrl, String id) {
-    List<String> urls = buildLiveLink(baseUrl, Collections.singletonList(id));
+    List<String> urls = buildLink(baseUrl, Collections.singletonList(id), PREVIEW_URL_SERVICE_URL);
     if(!urls.isEmpty()) {
       return urls.get(0);
     }
@@ -58,12 +39,22 @@ public class ContentLinkBuilder {
     return null;
   }
 
-  private List<String> buildLiveLink(String baseUrl, Iterable<String> ids) {
+  @Nullable
+  String buildLiveLink(String baseUrl, String id) {
+    List<String> urls = buildLink(baseUrl, Collections.singletonList(id), LIVE_URL_SERVICE_URL);
+    if(!urls.isEmpty()) {
+      return urls.get(0);
+    }
+
+    return null;
+  }
+
+  private List<String> buildLink(String baseUrl, Iterable<String> ids, String serviceUrlSuffix) {
     String fullUrl = baseUrl;
     if (!fullUrl.endsWith("/")) {
       fullUrl = fullUrl + "/";
     }
-    String serviceUrl = fullUrl + LIVE_URL_SERVICE_URL;
+    String serviceUrl = fullUrl + serviceUrlSuffix;
     UriComponents uriComponents = UriComponentsBuilder.fromUriString(serviceUrl).build();
 
     Collection<UrlServiceRequestParams> params = new ArrayList<>();
@@ -93,21 +84,6 @@ public class ContentLinkBuilder {
     }
 
     return links;
-  }
-
-  private String getLink(String serviceUrl) {
-    try {
-      URL url = new URL(serviceUrl);
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("GET");
-      connection.setDoInput(true);
-      connection.setUseCaches(false);
-      return IOUtils.toString(connection.getInputStream(), "utf-8");
-    } catch (Exception e) {
-      LOG.error("Failed to get links to CAE: {}", e.getMessage(), e);
-    }
-
-    return null;
   }
 
   private String postLinks(String serviceUrl, String body) {
